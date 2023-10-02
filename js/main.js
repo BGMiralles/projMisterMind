2f53ed8400148aeef430027c2895ccaa148710c0
Borja <pintocorreas@gmail.com>
2023-10-02 17:44:51 +0200

    Added: show select colors in game web

--- js/main.js
+++ js/main.js
@@ -8,3 +8,14 @@ for (let i = 0; i < colores.length; i++) {
   let randomIndex = Math.floor(Math.random() * colores.length);
   combinacionSecreta.push(colores[randomIndex]);
 }
+
+let coloresSecretosHTMLElements = document.querySelectorAll('.combsecreta .color-secreto');
+let coloresSecretosHTMLElementsArray = Array.from(coloresSecretosHTMLElements);
+coloresSecretosHTMLElementsArray.forEach((spanColor, index) => {
+  spanColor.style.backgroundColor = combinacionSecreta[index];
+});
+
+const coloresSeleccionados = document.querySelectorAll(".color-seleccionado");
+coloresSeleccionados.forEach((spanColor, index) => {
+  spanColor.style.backgroundColor = colores[index];
+});
